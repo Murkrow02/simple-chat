bb5b151548b730537ded140aecf2ca9c94ab627d
murkrow <marcocoppola02.mc@gmail.com>
2024-03-08 20:35:29 +0100

    Started startable chats popup

--- resources/views/chat-home.blade.php
+++ resources/views/chat-home.blade.php
@@ -18,12 +18,13 @@
         <div class="flex flex-row h-full">
 
             <!-- LEFT Started chats -->
-            <div class="w-1/4 h-full flex flex-col absolute md:relative md:translate-x-0 -translate-x-full ease-out duration-300">
+            <div id="slide-over"
+                 class="w-full lg:w-1/4 h-full flex flex-col absolute lg:relative lg:translate-x-0  ease-out duration-300 bg-white">
 
                 <!-- Header -->
                 <div class="flex flex-row items-center justify-between p-3 bg-primary text-white">
                     <h2 class="text-xl font-bold">Chats</h2>
-                    <a href="/chat/new" class="text-white">New chat</a>
+                    <button onclick="showNewChatModal()" class="text-white">New chat</button>
                 </div>
 
                 <!-- Chats -->
@@ -37,42 +38,78 @@
                 <x-simple-chat::chat-view/>
             </div>
 
+            <!-- New chat modal -->
+            <div id="new-chat-modal"
+                 class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+                <x-simple-chat::new-chat-modal/>
+            </div>
+
+
         </div>
-
-
-        <script>
-
-            let openedChat = document.getElementById('opened-chat');
-            function openChat(selectedChatCell) {
-
-                // Show opened chat
-                openedChat.hidden = false;
-
-                // Call function on chat view in order to detect new chat
-                currentChatChanged(selectedChatCell.getAttribute('data-chat-id'));
-
-                // Remove any previous cell with active background
-                let activeCell = document.querySelector('.active-chat-cell');
-                if (activeCell != null)
-                    activeCell.classList.remove('active-chat-cell');
-
-                // Add active background to selected cell
-                selectedChatCell.classList.add('active-chat-cell');
-            }
-
-
-            // Download started chats
-            let startedChatsDiv = document.getElementById('started-chats');
-            document.addEventListener("DOMContentLoaded", function () {
-                axios.get('/chat/chats')
-                    .then(response => {
-                        startedChatsDiv.innerHTML += response.data;
-                    });
-            });
-
-
-        </script>
-
     </div>
+
+
+    <script>
+
+        // Get elements
+        let openedChat = document.getElementById('opened-chat');
+        let slideOver = document.getElementById('slide-over');
+        let newChatModal = document.getElementById('new-chat-modal');
+
+
+        // Variables
+        let isLeftContainerOpen = true;
+
+        // Open in right container the chat with messages
+        function openChat(selectedChatCell) {
+
+            // Show opened chat
+            openedChat.hidden = false;
+
+            // Hide slide over
+            hideSlideOver();
+
+            // Call function on chat view in order to detect new chat
+            currentChatChanged(selectedChatCell.getAttribute('data-chat-id'));
+
+            // Remove any previous cell with active background
+            let activeCell = document.querySelector('.active-chat-cell');
+            if (activeCell != null)
+                activeCell.classList.remove('active-chat-cell');
+
+            // Add active background to selected cell
+            selectedChatCell.classList.add('active-chat-cell');
+        }
+
+
+        // Download started chats
+        let startedChatsDiv = document.getElementById('started-chats');
+        document.addEventListener("DOMContentLoaded", function () {
+            axios.get('/chat/chats')
+                .then(response => {
+                    startedChatsDiv.innerHTML += response.data;
+                });
+        });
+
+
+        // Show left slide over with started chats
+        function showSlideOver() {
+            slideOver.classList.remove('-translate-x-full');
+            isLeftContainerOpen = true;
+        }
+
+        // Hide left slide over with started chats
+        function hideSlideOver() {
+            slideOver.classList.add('-translate-x-full');
+            isLeftContainerOpen = false;
+        }
+
+        function showNewChatModal() {
+            newChatModal.style.display = "block";
+        }
+
+
+    </script>
+
 @endsection
 
--- resources/views/components/chat-view.blade.php
+++ resources/views/components/chat-view.blade.php
@@ -1,12 +1,14 @@
 <div class="flex-1 h-full flex flex-col">
 
     <!-- Chat header -->
-    <div class="p-3 bg-secondary text-white text-center">
+    <div class="p-3 bg-secondary text-white text-center flex flex-row justify-between lg:justify-center">
+        <button onclick="showSlideOver()" class="lg:hidden">Left</button>
         <h2 class="text-xl font-bold">{{__('simple-chat::chat.chat')}}</h2>
+        <p class="lg:hidden"></p>
     </div>
 
     <!-- Chat messages -->
-    <div class="flex-1 flex flex-col overflow-y-auto px-4" id="chat-messages">
+    <div class="flex-1 flex flex-col overflow-y-auto px-4 pb-3" id="chat-messages">
 
     </div>
 
